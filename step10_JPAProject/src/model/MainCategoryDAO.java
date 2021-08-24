086ac3c0ad80c6108650d831d6191d0eb0166db1
pt <ptkeb46@gmail.com>
2021-08-24 15:27:40 +0900

    bb

--- step10_JPAProject/src/model/MainCategoryDAO.java
+++ step10_JPAProject/src/model/MainCategoryDAO.java
@@ -1,5 +1,37 @@
 package model;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import model.dto.MainCategoryDTO;
+import model.dto.UserDTO;
+import model.util.Util;
+
 public class MainCategoryDAO {
 
+	public static MainCategoryDTO getCategory(int id) {
+		EntityManager em = Util.getEntityManager();
+		MainCategoryDTO category = em.createNamedQuery("category.findById",MainCategoryDTO.class)
+							.setParameter("mainCategoryId", id).getSingleResult();
+		return category;
+	}
+	
+	public static List<MainCategoryDTO> getAllCategory() {
+		EntityManager em = Util.getEntityManager();
+		List<MainCategoryDTO> category = em.createNamedQuery("category.findAllById",MainCategoryDTO.class)
+							.getResultList();
+		return category;
+	}
+	
+	public static void updateCategoryLibrarianId(int id, int librarianId) {
+		EntityManager em = Util.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		MainCategoryDTO category = em.find(MainCategoryDTO.class, id);
+		category.setLibrarianId(librarianId);
+		tx.commit();
+	}
+	
 }
