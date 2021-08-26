b80cac6b0fca3e1f086dd716842de7394f732dfd
zyan <bingbong-party>
2021-08-26 11:22:00 +0900

    update

--- step10_JPAProject/src/model/LibrarianDAO.java
+++ step10_JPAProject/src/model/LibrarianDAO.java
@@ -1,50 +1,44 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-import org.junit.jupiter.api.Test;
-
 import model.dto.LibrarianDTO;
-import model.dto.UserDTO;
 import model.util.Util;
 
 public class LibrarianDAO {
 	public static LibrarianDTO getLibrarian(int id) {
 		EntityManager em = Util.getEntityManager();
 
-		LibrarianDTO v = (LibrarianDTO) em.createNamedQuery("LIBRARIAN.findByLibrarianId")
-				.setParameter("librarianId", id).getSingleResult();
+		LibrarianDTO librarian = (LibrarianDTO)em.createNamedQuery("LIBRARIAN.findByLibrarianId").setParameter("librarianId", id).getSingleResult();
 
 		em.close();
 		em = null;
 
-		return v;
+		return librarian;
 	}
 
 	public static List<LibrarianDTO> getAllLibrarian() {
 		EntityManager em = Util.getEntityManager();
 
-		List<LibrarianDTO> v = em.createNamedQuery("LIBRARIAN.findAllByLibrarianId", LibrarianDTO.class)
-				.getResultList();
+		List<LibrarianDTO> librarian = em.createNamedQuery("LIBRARIAN.findAllByLibrarianId", LibrarianDTO.class).getResultList();
 
 		em.close();
 		em = null;
 
-		return v;
+		return librarian;
 	}
 
 	public static void addLibrarian(int id, String name, String offday) {
 		EntityManager em = Util.getEntityManager();
 		EntityTransaction tx = em.getTransaction();
+		
 		tx.begin();
 
 		LibrarianDTO Librarian = new LibrarianDTO(id, name, offday);
+		
 		em.persist(Librarian);
 
 		tx.commit();
@@ -81,10 +75,12 @@ public class LibrarianDAO {
 	public static void deleteLibrarian(int id) {
 		EntityManager em = Util.getEntityManager();
 		EntityTransaction tx = em.getTransaction();
+		
 		tx.begin();
 
 		LibrarianDTO librarian = em.find(LibrarianDTO.class, id);
 		em.remove(librarian);
+		
 		em.flush();
 		tx.commit();
 	}
--- step10_JPAProject/src/model/MainCategoryDAO.java
+++ step10_JPAProject/src/model/MainCategoryDAO.java
@@ -14,15 +14,15 @@ public class MainCategoryDAO {
 
 	public static MainCategoryDTO getCategory(String id) {
 		EntityManager em = Util.getEntityManager();
-		MainCategoryDTO category = em.createNamedQuery("category.findById", MainCategoryDTO.class)
-								   .setParameter("mainCategoryId", id).getSingleResult();
+		MainCategoryDTO category = em.createNamedQuery("category.findById", MainCategoryDTO.class).setParameter("mainCategoryId", id).getSingleResult();
+		
 		return category;
 	}
 
 	public static List<MainCategoryDTO> getAllCategory() {
 		EntityManager em = Util.getEntityManager();
-		List<MainCategoryDTO> category = em.createNamedQuery("category.findAllById", MainCategoryDTO.class)
-										 .getResultList();
+		List<MainCategoryDTO> category = em.createNamedQuery("category.findAllById", MainCategoryDTO.class).getResultList();
+		
 		return category;
 	}
 
@@ -31,7 +31,9 @@ public class MainCategoryDAO {
 		EntityTransaction tx = em.getTransaction();
 		List<LibrarianDTO> Lib = LibrarianDAO.getAllLibrarian();
 		ArrayList<Integer> list = new ArrayList<>();
+		
 		Lib.forEach(v -> list.add(v.getLibrarianId()));
+		
 		if (list.contains(librarianId)) {
 			tx.begin();
 			MainCategoryDTO category = em.find(MainCategoryDTO.class, id);
@@ -42,5 +44,4 @@ public class MainCategoryDAO {
 			System.out.println("존재하지 않는 사서번호 입니다.");
 		}
 	}
-
 }
